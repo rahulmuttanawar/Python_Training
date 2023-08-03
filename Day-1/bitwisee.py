337dcdcb3440b3f7f159dafb34709acf7d2829b2
rahulmuttanawar <rahul.muttanawar@genzeon.com>
2023-08-03 11:39:28 +0530

    Second commit

---
+++ Day-1/bitwisee.py
@@ -0,0 +1,26 @@
+x = 5
+y = 3
+result_and = x & y
+print(result_and)
+
+x = 5
+y = 3
+result_or = x | y
+print(result_or)
+
+x = 5
+result_not = ~x
+print(result_not)
+
+x = 5
+y = 3
+result_xor = x ^ y
+print(result_xor)
+
+x = 5
+shifted_left = x << 2
+print(shifted_left)
+
+x = 20
+shifted_right = x >> 2
+print(shifted_right)
---
+++ Day-1/slicingg.py
@@ -0,0 +1,14 @@
+my_list = [10, 20, 30, 40, 50, 60, 70]
+
+# Negative indexing
+print(my_list[-1])
+print(my_list[-2])
+
+# Slicing
+print(my_list[2:5])
+print(my_list[:3])
+print(my_list[4:])
+
+# Negative indexing with slicing
+print(my_list[-3:-1])
+print(my_list[-5:])
